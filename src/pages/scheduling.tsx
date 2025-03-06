e740036b4d30386ff75c485dd56fb44518aa03f0
Kauã Neves <kauanv06@gmail.com>
2025-03-06 16:35:06 -0300

    Connecting to the new features - API

--- src/pages/scheduling.tsx
+++ src/pages/scheduling.tsx
@@ -3,7 +3,6 @@ import { MdCalendarMonth } from "react-icons/md";
 import { HiIdentification } from "react-icons/hi2";
 import { IoMdPeople } from "react-icons/io";
 import { ImScissors } from "react-icons/im";
-import { FaClock } from "react-icons/fa6";
 import { TbCalendarClock } from "react-icons/tb";
 import { useEffect, useState } from "react";
 
@@ -16,9 +15,48 @@ interface VerifyProps {
   };
 }
 
+interface EmployeesProps {
+  id: number;
+  name: string;
+  phone: string;
+}
+
+interface InfoProps {
+  id: number;
+  name: string;
+  price: number;
+}
+
 export default function Scheduling() {
   const [stateVerify, setStateVerify] = useState(true);
   const [data, setData] = useState({} as VerifyProps);
+  const [employees, setEmployees] = useState([] as EmployeesProps[]);
+  const [selectedEmployee, setSelectedEmployee] = useState(0);
+  const [date, setDate] = useState<string | null>(null);
+  const [employeeServiceData, setemployeeServiceData] = useState([]);
+  const [selectedService, setSelectedService] = useState("");
+
+  function formatDate(date: string) {
+    const [year, month, day] = date.split("-");
+    return `${day}/${month}/${year}`;
+  }
+
+  useEffect(() => {
+    async function getService() {
+      const response = await fetch("http://localhost:3000/getServicesSpecify", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: selectedEmployee }),
+      });
+
+      const data = await response.json();
+      setemployeeServiceData(data);
+    }
+
+    getService();
+  }, [selectedEmployee]);
 
   useEffect(() => {
     async function verifyState() {
@@ -34,7 +72,14 @@ export default function Scheduling() {
       }
     }
 
+    async function getEmployees() {
+      const response = await fetch("http://localhost:3000/getEmployees");
+      const data = await response.json();
+      setEmployees(data);
+    }
+
     verifyState();
+    getEmployees();
   }, []);
 
   return (
@@ -50,36 +95,58 @@ export default function Scheduling() {
               </h1>
               <div className="flex flex-col space-y-7 mt-4">
                 <div className="relative bg-gray-300 rounded drop-shadow-md">
-                  <input
-                    type="text"
-                    className="pl-10 pr-4 py-2 rounded-lg w-full font-bold focus:outline-none"
-                    placeholder="Profissional"
-                  />
-                  <IoMdPeople className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                  <div className="relative">
+                    <IoMdPeople className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                    <select
+                      value={selectedEmployee}
+                      onChange={(e) =>
+                        setSelectedEmployee(Number(e.target.value))
+                      }
+                      className="pl-10 pr-4 py-2 rounded-lg w-full font-bold focus:outline-none bg-gray-300 text-gray-500"
+                    >
+                      <option value={0} disabled selected hidden>
+                        Selecione um Profissional
+                      </option>
+                      {employees.map((employee) => {
+                        return (
+                          <option key={employee.id} value={employee.id}>
+                            {employee.name}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </div>
                 </div>
                 <div className="relative bg-gray-300 rounded drop-shadow-md">
-                  <input
-                    type="text"
-                    className="pl-10 pr-4 py-2 rounded-lg w-full font-bold focus:outline-none"
-                    placeholder="Serviço"
-                  />
-                  <ImScissors className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                  <div className="relative">
+                    <ImScissors className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
+                    <select
+                      value={selectedService}
+                      onChange={(e) => setSelectedService(e.target.value)}
+                      className="pl-10 pr-4 py-2 rounded-lg w-full font-bold focus:outline-none bg-gray-300 text-gray-500"
+                    >
+                      <option value={""} disabled selected hidden>
+                        Selecione o(s) serviços
+                      </option>
+                      {employeeServiceData.map((info: InfoProps) => {
+                        return (
+                          <option key={info.id} value={info.name}>
+                            {info.name}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </div>
                 </div>
                 <div className="relative bg-gray-300 rounded drop-shadow-md">
                   <input
                     type="date"
+                    value={date || ""}
+                    onChange={(e) => setDate(e.target.value)}
                     className="pl-10 pr-4 py-2 rounded-lg w-full font-bold focus:outline-none text-gray-500"
                     placeholder="Data e Horário"
                   />
                   <MdCalendarMonth className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                </div>
-                <div className="relative bg-gray-300 rounded drop-shadow-md">
-                  <input
-                    type="text"
-                    className="pl-10 pr-4 py-2 rounded-lg w-full font-bold focus:outline-none"
-                    placeholder="Horário"
-                  />
-                  <FaClock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
                 </div>
               </div>
             </div>
@@ -97,19 +164,23 @@ export default function Scheduling() {
                 </div>
                 <div className="relative">
                   <h1 className="pl-10 pr-4 py-2 rounded-lg w-full font-bold text-gray-500 focus:outline-none">
-                    Profissional
+                    {selectedEmployee
+                      ? employees.find(
+                          (employee) => employee.id === selectedEmployee
+                        )?.name
+                      : "Profissional"}
                     <IoMdPeople className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
                   </h1>
                 </div>
                 <div className="relative">
                   <h1 className="pl-10 pr-4 py-2 rounded-lg w-full font-bold text-gray-500 focus:outline-none">
-                    Serviços
+                    {selectedService === "" ? "Serviços" : selectedService}
                     <ImScissors className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
                   </h1>
                 </div>
                 <div className="relative">
                   <h1 className="pl-10 pr-4 py-2 rounded-lg w-full font-bold text-gray-500 focus:outline-none">
-                    Data e Horário
+                    {date ? formatDate(date) : "Data e Horário"}
                     <TbCalendarClock className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500" />
                   </h1>
                 </div>
